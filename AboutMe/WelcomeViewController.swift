eb516312081d11dca7ece0a3df38bdd26bcce282
Ilya <fekl13in@gmail.com>
2025-02-10 23:34:38 +0300

    Adding Properties

---
+++ AboutMe/WelcomeViewController.swift
@@ -0,0 +1,24 @@
+//
+//  WelcomeViewController.swift
+//  AboutMe
+//
+//  Created by Ilya Feklin on 10.02.2025.
+//
+
+import UIKit
+
+final class WelcomeViewController: UIViewController {
+
+    @IBOutlet var userNameLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+     
+    }
+    
+    @IBAction func LogOutAction() {
+    }
+    
+}
