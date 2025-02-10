891f9d6e42ee79b2c9fa9ab053a9e81071e2a5a2
Ilya <fekl13in@gmail.com>
2025-02-10 07:59:48 +0300

    Initial Commit

---
+++ AboutMeTests/AboutMeTests.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeTests.swift
+//  AboutMeTests
+//
+//  Created by Ilya Feklin on 10.02.2025.
+//
+
+import Testing
+@testable import AboutMe
+
+struct AboutMeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
